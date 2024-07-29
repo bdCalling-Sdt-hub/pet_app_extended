dd743876a1da09030da80bd6e173416f7701c773
Siam <siambci@gmail.com>
2024-07-29 14:09:40 +0600

    api integration starting

---
+++ lib/Controllers/common_controller/auth/change_password_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+
+class ChangePasswordController extends GetxController {
+  bool isLoading = false;
+
+  TextEditingController currentPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  // Future<void> changePasswordRepo() async {
+  //   Get.back();
+  //   return;
+  //   isLoading = true;
+  //   update();
+  //
+  //   Map<String, String> body = {
+  //     "oldPassword": currentPasswordController.text,
+  //     "newPassword": newPasswordController.text
+  //   };
+  //   var response =
+  //       await ApiService.patchApi(AppUrls.changePassword, body: body);
+  //
+  //   if (response.statusCode == 200) {
+  //     Utils.toastMessage(response.message);
+  //
+  //     currentPasswordController.clear();
+  //     newPasswordController.clear();
+  //     confirmPasswordController.clear();
+  //
+  //     Get.back();
+  //   } else {
+  //     Get.snackbar(response.statusCode.toString(), response.message);
+  //   }
+  //   isLoading = false;
+  //   update();
+  // }
+}
---
+++ lib/Controllers/common_controller/setting/setting_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+
+// class SettingController extends GetxController {
+//   TextEditingController passwordController = TextEditingController();
+//
+//   bool isLoading = false;
+//
+//   deleteAccountRepo() async {
+//     isLoading = true;
+//     update();
+//
+//     var body = {"password": passwordController.text};
+//
+//     var response = await ApiService.deleteApi(AppUrls.user, body: body);
+//
+//     if (response.statusCode == 200) {
+//       Get.offAllNamed(AppRoutes.signIn);
+//     } else {
+//       Utils.snackBarMessage(response.statusCode.toString(), response.message);
+//     }
+//     isLoading = false;
+//     update();
+//   }
+// }
