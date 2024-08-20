73010d2df2a5d1692697b767bf391e4804f55bf4
Siam <siambci@gmail.com>
2024-08-20 19:29:14 +0600

    code structure changed

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,10 +1,20 @@
 
+import 'package:felpus/views/screens/adoption/adoption_view.dart';
+import 'package:felpus/views/screens/adoption/all_adoption_pets_view.dart';
+import 'package:felpus/views/screens/adoption/create_adoption_view.dart';
 import 'package:get/get.dart';
 
 class AppRoutes{
   static const String splashScreen = "/splash_screen.dart";
 
+  static const String adoptionView = "/adoption_view.dart";
+  static const String allAdoptionPetsView = "/all_adoption_pets_view.dart";
+  static const String createAdoptionView = "/create_adoption_view.dart";
+
   static List<GetPage> routes = [
+    GetPage(name: adoptionView, page: () => const AdoptionView()),
+    GetPage(name: allAdoptionPetsView, page: () => const AllAdoptionPetsView()),
+    GetPage(name: createAdoptionView, page: () => const CreateAdoptionView()),
     // GetPage(name: splashScreen, page: () => SplashScreen()),
   ];
 }
